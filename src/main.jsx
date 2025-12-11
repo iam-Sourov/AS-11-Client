19952be9b321cd227b41b56a620109fa0f0ec067
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-12-11 13:36:15 +0600

    Deployment Problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AuthProvider from './contexts/AuthContext'
+import AuthProvider from './contexts/AuthProvider'
 import router from './Routes/Routes'
 import { RouterProvider } from 'react-router'
 
